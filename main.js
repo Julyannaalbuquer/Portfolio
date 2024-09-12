811540fc6fc9f756cf490c6c6f0ffa7849e5c9bd
Julyannaalbuquer <julyannaaraujoalbuquerque@gmail.com>
2024-09-12 19:09:25 -0300

    Updating JS

--- main.js
+++ main.js
@@ -1,17 +1,4 @@
-
-document.getElementById('btn-certificado').addEventListener('click', function() {
-    const pdfUrl = './assets/Profile.pdf';
-
-    const a = document.createElement('a');
-    a.href = pdfUrl;
-    a.download = 'Currículo.pdf';
-
-    document.body.appendChild(a);
-    a.click();
-    document.body.removeChild(a);
-});
-
-// Alterna a visibilidade do menu no clique do ícone de hambúrguer
+// Função do menu hambúrguer
 const hamburger = document.getElementById("hamburger");
 const navbarList = document.getElementById("navbar-list");
 const closeBtn = document.getElementById("close-btn");
@@ -29,4 +16,27 @@ hamburger.addEventListener("click", () => {
 closeBtn.addEventListener("click", () => {
     navbarList.classList.remove("open");
     closeBtn.style.display = "none"; // Esconde o botão de fechar quando o menu fecha
+});
+
+// Fecha o menu ao clicar fora dele
+document.addEventListener("click", function(event) {
+    const isClickInside = navbarList.contains(event.target) || hamburger.contains(event.target) || closeBtn.contains(event.target);
+
+    if (!isClickInside) {
+        navbarList.classList.remove("open");
+        closeBtn.style.display = "none"; 
+    }
+});
+
+// Função baixar currículo em PDF
+document.getElementById('btn-curriculo').addEventListener('click', function() {
+    const pdfUrl = './assets/Profile.pdf';
+
+    const a = document.createElement('a');
+    a.href = pdfUrl;
+    a.download = 'Currículo.pdf';
+
+    document.body.appendChild(a);
+    a.click();
+    document.body.removeChild(a);
 });
