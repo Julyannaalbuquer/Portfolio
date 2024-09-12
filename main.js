6b143fc28a33fb9902d78069d3da40c3b1950ce6
Julyannaalbuquer <julyannaaraujoalbuquerque@gmail.com>
2024-09-12 00:36:18 -0300

    Atualização

--- main.js
+++ main.js
@@ -11,5 +11,27 @@ document.getElementById('btn-certificado').addEventListener('click', function() 
     document.body.removeChild(a);
 });
 
+// Alterna a visibilidade do menu no clique do ícone de hambúrguer
+const hamburger = document.getElementById("hamburger");
+const navbarList = document.getElementById("navbar-list");
+const closeBtn = document.getElementById("close-btn");
+
+hamburger.addEventListener("click", () => {
+    navbarList.classList.toggle("open");
+    if (navbarList.classList.contains("open")) {
+        closeBtn.style.display = "block";
+    } else {
+        closeBtn.style.display = "none";
+    }
+});
+
+// Fecha o menu ao clicar no botão de fechar
+closeBtn.addEventListener("click", () => {
+    navbarList.classList.remove("open");
+    closeBtn.style.display = "none"; // Esconde o botão de fechar quando o menu fecha
+});
+
+const statusTexto = document.getElementById('status');
+
 
 
